1c12f99f074043b233103471ffa2b81e8c47132d
Michael Harrison <mharrison13@cnm.edu>
2017-04-13 08:36:56 -0600

    Phase 0 - Persona

--- epic/persona.php
+++ epic/persona.php
@@ -3,6 +3,7 @@
 <title>data-design persona</title>
 </head>
 <body>
+	<p><img src="http://"</p>
 	<p><strong>Name:</strong> Emanuel R. Lombard</p>
 	<p><strong>Age:</strong> 70</p>
 	<br>
@@ -12,5 +13,5 @@
 	<br>
 	<p><strong>Attitudes and Behavior: </strong>Retired with plenty of time on his hands, Emanuel is very technically savy for a 70 year old. His videos often go viral with his in-depth reviews of online purchases. He spends much of his time looking for new places to purchase his next video review. Emanuel is not very organized and often has a hard time remembering many of the items he comes across and relies heavily on the ability to save, bookmark and favorite the items he comes across.</p>
 	<br>
-	<p><strong>Frustrations and Needs: </strong> Emanuel needs a way to favorite items of interest on <em>Very Bad Etsy</em> as he has no way of keeping track potential products for his video reviews. He also wants to build his </p>
+	<p><strong>Frustrations and Needs: </strong> Emanuel needs a way to favorite items of interest on <em>Very Bad Etsy</em> as he has no way of keeping track potential products for his video reviews. He also wants to build his <em>Very Bad Etsy</em> profile so that others can see the products that he has selected as favorites. </p>
 </body>
