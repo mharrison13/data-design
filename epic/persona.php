4262449d3c1a40052bab35b8e6ad3bebf255b361
Michael Harrison <mharrison13@cnm.edu>
2017-04-13 08:54:31 -0600

    Phase 0 - Persona rev2

--- epic/persona.php
+++ epic/persona.php
@@ -4,9 +4,8 @@
 </head>
 <body>
 	<p><img src="http://"</p>
-	<p><strong>Name:</strong> Emanuel R. Lombard</p>
-	<p><strong>Age:</strong> 70</p>
-	<br>
+	<p><strong>Name:</strong> Emanuel R. Lombard<br>
+		<strong>Age:</strong> 70</p>
 	<p><strong>Profession: </strong>Avid <em>Very Bad Etsy</em> user, and retired electronic reporter spends all of his time purchasing items online and sharing them on the social media site YouTube.</p>
 	<p><Strong>Technology: </Strong>Mac user. Uses a Mac Desktop computer with a 25" samsung monitor at home, a 15 inch Mac Book Pro while out and about and an iPhone 7 with 4G connectivity.</p>
 	<p><strong>Attitudes and Behavior: </strong>Retired with plenty of time on his hands, Emanuel is very technically savy for a 70 year old. His videos often go viral with his in-depth reviews of online purchases. He spends much of his time looking for new places to purchase his next video review. Emanuel is not very organized and often has a hard time remembering many of the items he comes across and relies heavily on the ability to save, bookmark and favorite the items he comes across.</p>
