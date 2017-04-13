73aa9d339d0817ca88476fdac8d49822cef7649c
Michael Harrison <mharrison13@cnm.edu>
2017-04-13 09:22:37 -0600

    Phase 0 - Persona rev2

--- epic/use-case.php
+++ epic/use-case.php
@@ -6,6 +6,7 @@
 	<h1>Use Case</h1>
 	<h2>A Very Bad Etsy Site</h2>
 	<main>
-		<p>As a Very Bad Etsy user, I want to favorite an item in order to share with the world what an amazing product I found and to come back to the product at a later time to purchase.  </p>
+		<p>Emanuel is a <em>Very Bad Etsy</em> user. Retired with lots of time on his hands he has taken to making viral YouTube videos where can reviews products he purchases online as a way to generate ad revenue.</p>
+		<p>Three days ago, Emanuel spent 4 hours searching for products for upcoming video reviews. This morning, he revisited his <em>Very Bad Etsy</em> account and realised he has no way to go back to the products he wants to purchase. Emanuel has a poor memory and can never seem to find the pages he previously came across to purchase.</p>
 	</main>
 </body>
