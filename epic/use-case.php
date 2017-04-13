fa13a6dfec597743627a474ddb0c55e19ff40d86
Michael Harrison <mharrison13@cnm.edu>
2017-04-13 11:13:54 -0600

    Phase 0 - conceptual-model rev

--- epic/use-case.php
+++ epic/use-case.php
@@ -7,13 +7,13 @@
 	<h2>A Very Bad Etsy Site</h2>
 	<main>
 		<p>Three days ago, Emanuel spent 4 hours searching for products from his new Mac Book Pro on Mozilla Firefox for his next set of upcoming video reviews. He hand wrote of list of potential products that caught his interest and bookmarked the main website.</p>
-		<p>This morning, he revisited his <em>Very Bad Etsy</em> account and realised he has no way to go back to the products he wants to purchase. Emanuel has a poor memory and can never seem to find the pages he previously came across to purchase. Emanuel would like to have a way of selecting to favorite items to view and purchase at a later date.</p>
+		<p>This morning, he revisited his <em>Very Bad Etsy</em> account and realised he has no way to go back to the products he wants to purchase. Emanuel has a poor memory and can never seem to find the pages he previously came across to purchase. Emanuel would like to have a way of selecting favorite on items to view and purchase at a later date.</p>
 		<p>With a very large following, Emanuel also wants his followers to have access to the products he has selected to favorite on <em>Very Bad Etsy</em>.</p>
 		<h2>Interaction Flow</h2>
 		<ol>
 			<li>User logs into site</li>
 			<li>VeryBadEtsy.com delivers profile landing page</li>
-			<li>user searches product</li>
+			<li>User searches product</li>
 			<li>VeryBadEtsy.com delivers search results</li>
 			<li>User clicks <em>favorite</em> on product.</li>
 			<li>VeryBadEtsy.com adds product to User's <em>favorited items</em></li>
