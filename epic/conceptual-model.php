daf845b9e75ab8c45c617bdeeb06953f0e701f4b
Michael Harrison <mharrison13@cnm.edu>
2017-04-13 10:04:32 -0600

    Phase 0 - conceptual-model rev

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -1,7 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Mike
- * Date: 4/12/17
- * Time: 4:46 PM
- */
+<!DOCTYPE html>
+<head>
+	<title>Conceptual-Model</title>
+</head>
+<body>
+	<main>
+		<h2>Entities and Attributes</h2>
+		<p><strong>Profile</strong></p>
+		<ul>
+		<li>profileID (primary key)</li>
+		<li>profileActivationToken</li>
+		<li>profileAtHandle</li>
+		<li>profileEmail</li>
+		<li>profileHash</li>
+		<li>profilePhone</li>
+		<li>profileSalt</li>
+		</ul>
+		<p><strong>Favorite</strong></p>
+	<ul>
+		<li>favoriteID</li>
+		<li>favoriteProfileID</li>
+		<li>favoriteDate</li>
+</ul>
+		<p><strong>Relations</strong></p>
+		<ul>
+			<li>One <strong>Profile </strong>favorites product - (m to n)</li>
+		</ul>
+	</main>
+</body>
