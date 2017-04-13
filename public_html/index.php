e35fe422b3a76fdc0d429ea532636df19fff183e
Michael Harrison <mharrison13@cnm.edu>
2017-04-13 08:43:35 -0600

    Phase 0 - Persona

--- public_html/index.php
+++ public_html/index.php
@@ -1,7 +1,7 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Mike
- * Date: 4/12/17
- * Time: 4:48 PM
- */
+<!DOCTYPE html>
+	<head>
+		<title>test</title>
+	</head>
+	<body>
+		<main>test</main>
+	</body>
