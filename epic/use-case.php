fd6fa709be04f0fab98e5b55c81a89f2445db32e
Michael Harrison <mharrison13@cnm.edu>
2017-04-13 08:58:23 -0600

    Phase 0 - Persona rev2

--- epic/use-case.php
+++ epic/use-case.php
@@ -6,6 +6,6 @@
 	<h1>Use Case</h1>
 	<h2>A Very Bad Etsy Site</h2>
 	<main>
-		<p>As a Very Bad Etsy user, I want to favorite an item in order to share with the world what an amazing product I found and to come back to the product at a later time.  </p>
+		<p>As a Very Bad Etsy user, I want to favorite an item in order to share with the world what an amazing product I found and to come back to the product at a later time to purchase.  </p>
 	</main>
 </body>
