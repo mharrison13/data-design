f5c2f25c71949be27ebba4b6d06bece1ea747663
Michael Harrison <mharrison13@cnm.edu>
2017-04-13 10:46:51 -0600

    Phase 0 - conceptual-model rev

--- epic/use-case.php
+++ epic/use-case.php
@@ -9,6 +9,16 @@
 		<p>Three days ago, Emanuel spent 4 hours searching for products from his new Mac Book Pro on Mozilla Firefox for his next set of upcoming video reviews. He hand wrote of list of potential products that caught his interest and bookmarked the main website.</p>
 		<p>This morning, he revisited his <em>Very Bad Etsy</em> account and realised he has no way to go back to the products he wants to purchase. Emanuel has a poor memory and can never seem to find the pages he previously came across to purchase.</p>
 		<p>With a very large following, Emanuel also wants his followers to have access to the products he has selected to favorite on <em>Very Bad Etsy</em>.</p>
+		<h2>Interaction Flow</h2>
+		<ol>
+			<li>log into site</li>
+			<li>VeryBadEtsy.com delivers profile landing page</li>
+			<li>user searches product</li>
+			<li>VeryBadEtsy.com delivers search results</li>
+			<li>User clicks <em>favorite</em> on product.</li>
+			<li>Product is added to Users <em>favorited items</em></li>
+
+		</ol>
 
 	</main>
 </body>
