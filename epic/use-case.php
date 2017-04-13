51bba01aeacd634afac60665678421e23431c2e1
Michael Harrison <mharrison13@cnm.edu>
2017-04-13 09:36:30 -0600

    Phase 0 - Persona rev2

--- epic/use-case.php
+++ epic/use-case.php
@@ -8,7 +8,7 @@
 	<main>
 		<p>Emanuel is a <em>Very Bad Etsy</em> user. Retired with lots of time on his hands he has taken to making viral YouTube videos where can reviews products he purchases online as a way to generate ad revenue.</p>
 		<p>Three days ago, Emanuel spent 4 hours searching for products for upcoming video reviews. This morning, he revisited his <em>Very Bad Etsy</em> account and realised he has no way to go back to the products he wants to purchase. Emanuel has a poor memory and can never seem to find the pages he previously came across to purchase.</p>
-		<p>With a very large following, Emanuel also wants his followers to have access to the products he has favorited on <em>Very Bad Etsy</em>.</p>
+		<p>With a very large following, Emanuel also wants his followers to have access to the products he has selected to favorite on <em>Very Bad Etsy</em>.</p>
 
 	</main>
 </body>
