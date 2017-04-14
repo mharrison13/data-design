569ed7df381e95084d84b35e1a413c0be3817b65
Michael Harrison <mharrison13@cnm.edu>
2017-04-14 12:39:32 -0600

    data-design .sql file

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -3,7 +3,6 @@ DROP TABLE IF EXISTS favorite;
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS profile;
 
--- time to creat the table!
 CREATE TABLE profile (
 	profileID INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	profileActivationToken CHAR(32),
@@ -12,18 +11,18 @@ CREATE TABLE profile (
 	profileHash CHAR(128) NOT NULL,
 	profilePhone VARCHAR(32),
 	profileSalt CHAR(64) NOT NULL,
-	UNIQUE (profileEmail),
-	UNIQUE (profileAtHandle),
-	PRIMARY KEY (profileID)
+	UNIQUE(profileEmail),
+	UNIQUE(profileAtHandle),
+	PRIMARY KEY(profileID)
 );
 
 CREATE TABLE product (
 	productID INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	productProfileID INT UNSIGNED NOT NULL,
-	productPrice VARCHAR (32),
-	INDEX(productProfileID)
-	FOREIGN KEY (productProfileID) REFERENCES profile(proFileId),
-	PRIMARY KEY (productID)
+	productPrice VARCHAR(32),
+	INDEX(productProfileID),
+	FOREIGN KEY(productProfileID) REFERENCES profile(profileId),
+	PRIMARY KEY(productID)
 );
 
 CREATE TABLE favorite (
@@ -32,6 +31,7 @@ CREATE TABLE favorite (
 	favoriteDate DATETIME(6) NOT NULL,
 	INDEX(favoriteID),
 	INDEX(favoriteProfileID),
-	FOREIGN KEY()
-
-)
+	FOREIGN KEY(favoriteID) REFERENCES profile(profileID),
+	FOREIGN KEY(favoriteProfileID) REFERENCES product(productID),
+	PRIMARY KEY(favoriteID, favoriteProfileID)
+);
