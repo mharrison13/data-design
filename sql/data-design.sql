771b8c7e4f8952d14920802fd8d6573b5558a375
Michael Harrison <mharrison13@cnm.edu>
2017-04-14 12:01:44 -0600

    data-design .sql file

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -14,5 +14,24 @@ CREATE TABLE profile (
 	profileSalt CHAR(64) NOT NULL,
 	UNIQUE (profileEmail),
 	UNIQUE (profileAtHandle),
-	PRIMARY key (profileID)
+	PRIMARY KEY (profileID)
 );
+
+CREATE TABLE product (
+	productID INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	productProfileID INT UNSIGNED NOT NULL,
+	productPrice VARCHAR (32),
+	INDEX(productProfileID)
+	FOREIGN KEY (productProfileID) REFERENCES profile(proFileId),
+	PRIMARY KEY (productID)
+);
+
+CREATE TABLE favorite (
+	favoriteID INT UNSIGNED NOT NUll,
+	favoriteProfileID INT UNSIGNED NOT NULL,
+	favoriteDate DATETIME(6) NOT NULL,
+	INDEX(favoriteID),
+	INDEX(favoriteProfileID),
+	FOREIGN KEY()
+
+)
