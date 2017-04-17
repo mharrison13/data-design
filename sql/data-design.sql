70dd407ae05c86f8177068a9d4435d081c458f73
Michael Harrison <mharrison13@cnm.edu>
2017-04-17 07:49:21 -0600

    data-design .sql file

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,10 +1,9 @@
--- this is my database
 DROP TABLE IF EXISTS favorite;
 DROP TABLE IF EXISTS product;
 DROP TABLE IF EXISTS profile;
 
 CREATE TABLE profile (
-	profileID INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	profileId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	profileActivationToken CHAR(32),
 	profileAtHandle VARCHAR(32) NOT NULL,
 	profileEmail VARCHAR(128) NOT NULL,
@@ -17,11 +16,11 @@ CREATE TABLE profile (
 );
 
 CREATE TABLE product (
-	productID INT UNSIGNED AUTO_INCREMENT NOT NULL,
+	productId INT UNSIGNED AUTO_INCREMENT NOT NULL,
 	productProfileID INT UNSIGNED NOT NULL,
 	productPrice VARCHAR(32),
 	INDEX(productProfileID),
-	FOREIGN KEY(productProfileID) REFERENCES profile(profileId),
+	FOREIGN KEY(productProfileId) REFERENCES profile(profileId),
 	PRIMARY KEY(productID)
 );
 
