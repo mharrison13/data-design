9cab71feb51baeed0298af2a1828615805e31946
Michael Harrison <mharrison13@cnm.edu>
2017-05-05 00:12:23 -0600

    updated sign up

--- public_html/api/sign-up/index.php
+++ public_html/api/sign-up/index.php
@@ -5,7 +5,7 @@ require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 use VerybadetsyHttp\DataDesign;
 /**
- * api for signing up too DDC Twitter
+ * api for signing up to Very Bad Etsy
  *
  * @author Gkephart <GKephart@cnm.edu>
  **/
